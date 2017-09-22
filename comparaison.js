ff7130c9e2c3168f7ca8a13aa6f290f9bd952a47
Leprince2 <31913966+Leprince2@users.noreply.github.com>
2017-09-22 11:50:22 +0200

    Add files via upload

---
+++ comparaison.js
@@ -0,0 +1,6 @@
+/* 
+Exercice : comparer deux nombres
+*/var nombre1 = Number(prompt("Entrez un nombre 1 :"));var nombre2 = Number(" Entrez un nombre 2 :"));
+if (nombre1 < nombre2) {console.log(nombre 1 + " est plud petit que "+ nombre2);
+} else if (nombre1 > nombre2) {console.log(nombre 1 + " est plud petit que "+ nombre2);
+} else {console.log(nombre 1 + " et " + nombre2 + " sont égaux");}
